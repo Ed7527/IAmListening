e3014036ff1dc047ceee6da6d08b0d1ca48b8a4b
Eddie <edward.torpy@my.jcu.edu.au>
2017-06-29 20:52:07 +1000

    EoD

--- app/src/main/java/com/edwardtorpy/iamlistening/MainActivity.java
+++ app/src/main/java/com/edwardtorpy/iamlistening/MainActivity.java
@@ -1,5 +1,8 @@
 package com.edwardtorpy.iamlistening;
 
+import android.Manifest;
+import android.media.MediaRecorder;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +10,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void stopSound (View view) {
-        Toast.makeText(this, "STOP!", Toast.LENGTH_SHORT).show();
+    private MediaRecorder mRecorder;
+
+    // Requesting permission to record audio
+    private boolean permissionToRecordAudioGranted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void recordSound(View view) {
@@ -17,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void playSound(View view) {
         Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stopSound (View view) {
+        Toast.makeText(this, "STOP!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
