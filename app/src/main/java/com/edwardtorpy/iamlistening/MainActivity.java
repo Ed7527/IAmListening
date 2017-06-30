f1393ed6fab5176614a6c02b75b0f40be98329db
Eddie <edward.torpy@my.jcu.edu.au>
2017-06-30 22:46:45 +1000

    Ask and check for audio recording permission

--- app/src/main/java/com/edwardtorpy/iamlistening/MainActivity.java
+++ app/src/main/java/com/edwardtorpy/iamlistening/MainActivity.java
@@ -1,8 +1,11 @@
 package com.edwardtorpy.iamlistening;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +17,26 @@ public class MainActivity extends AppCompatActivity {
 
     // Requesting permission to record audio
     private boolean permissionToRecordAudioGranted = false;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    public void requestPermissions (Activity activity, String [] permissions, int requestCode) {
+
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAudioGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+
+        if (!permissionToRecordAudioGranted){
+            finish();
+        }
     }
 
     public void recordSound(View view) {
@@ -37,5 +55,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
     }
 }
